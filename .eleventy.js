23c9ab9d4234e1e3c3147f93c5983867db943fd6
Veronika Jermolina <veronika.jermolina1@nhs.net>
2026-04-24 14:52:32 +0100

    Switch output to docs for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,11 @@ module.exports = function(eleventyConfig) {
   // Pass the CSS folder through unchanged
   eleventyConfig.addPassthroughCopy("public");
 
-  return {
+    return {
     dir: {
-      input: ".",
-      includes: "_includes",
-      output: "_site"
+        input: ".",
+        output: "docs",
+        includes: "_includes"
     }
-  };
+    };
 };
